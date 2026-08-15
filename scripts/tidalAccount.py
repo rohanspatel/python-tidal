ef43f982dbbc23cc2183513293549fca8971e858
Rohan Patel <85042499+rohanspatel@users.noreply.github.com>
2026-08-15 23:20:45 +0100

    Trim trailing whitespace

--- scripts/tidalAccount.py
+++ scripts/tidalAccount.py
@@ -23,12 +23,12 @@ class TidalAccount():
         # Try to restore session, or create a new one if the restoration fails
         if not self._restore_session():
             self._new_session()
-        
+
         # Sanity check on login
         if not self._session.check_login():
             logging.error("Could not log in to Tidal. Exiting...")
             exit(1)
-        
+
         self.account_information()
 
     def _restore_session(self) -> bool:
@@ -66,17 +66,17 @@ class TidalAccount():
             if self._session.check_login():
                 logging.info("Restored Tidal session from saved credentials")
                 return True
-        
+
         else:
             logging.debug("Credentials file not found")
 
         return False
-    
+
     def _save_session(self) -> None:
         """ Save the current session to the credentials .json file """
 
         logging.info(f"Saving OAuth credentials to {self._credentials}...")
-        
+
         try:
             creds = {
                 'token_type': self._session.token_type,
@@ -88,10 +88,10 @@ class TidalAccount():
             with open(self._credentials, 'w') as f:
                 json.dump(creds, f)
                 logging.debug(f"Written credentials to {f.name}")
-        
+
         except Exception as e:
             logging.error(f"Error saving credentials: {e}")
-    
+
     def _new_session(self) -> None:
         """ Create a new Tidal session via OAuth, store credentials for future use """
 
@@ -102,11 +102,11 @@ class TidalAccount():
         if self._session.check_login():
             logging.info("Sucessfully created new Tidal session")
             self._save_session()
-        
+
         else:
             logging.error("Failed to create new Tidal session. Exiting...")
             exit(1)
-        
+
     def account_information(self) -> None:
         """ Print basic account information to the log """
 
